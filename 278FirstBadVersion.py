5ca84481de180b7d8d06c5be6a84b4c5fad0835a
abedm <abedmohammed353@gmail.com>
2023-07-11 14:46:24 -0400

    Solved 278

--- 278FirstBadVersion.py
+++ 278FirstBadVersion.py
@@ -5,7 +5,25 @@
 
 class Solution(object):
     def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+        left = 1
+        right = n
+        bad_version = 0
+        while (left <= right):
+            mid = (left + right) // 2
+            is_bad = self.isBadVersion(mid)
+
+            if(is_bad):
+                bad_version = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+                
+        return bad_version
+    
+    def isBadVersion(self, n):
+        if(n == 4):
+            return True
+        return False
+
+answer = Solution()
+print(answer.firstBadVersion(5))
