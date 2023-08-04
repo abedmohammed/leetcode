4cbcb53c2dbade95a6d48de3c8a80ec702a5d673
Mohammed Abed <abedmohammed353@gmail.com>
2023-08-03 20:45:05 -0400

    Solved 133

--- 133CloneGraph.py
+++ 133CloneGraph.py
@@ -1,4 +1,4 @@
-# Definition for a Node.
+from collections import deque
 class Node(object):
     def __init__(self, val = 0, neighbors = None):
         self.val = val
@@ -6,8 +6,47 @@ class Node(object):
 
 
 class Solution(object):
+    # def cloneGraph(self, node):
+    #     visited = {}
+    #     copiedNodes = {}
+    #     queue = deque()
+    #     queue.append(node)
+    #     copiedNodes[node.val] = Node(node.val)
+    #     visited[node.val] = 1
+
+
+    #     while queue:
+    #         currentNode = queue.popleft()
+    #         value = currentNode.val
+    #         newNode = copiedNodes[value]
+
+    #         for i in range(len(currentNode.neighbors)):
+    #             neighbhor = currentNode.neighbors[i]
+    #             if neighbhor.val not in copiedNodes:
+    #                 copiedNodes[neighbhor.val] = Node(neighbhor.val)
+    #             newNode.neighbors.append(copiedNodes[neighbhor.val])
+    #             if neighbhor.val not in visited:
+    #                 queue.append(neighbhor)
+    #                 visited[neighbhor.val] = 1
+        
+    #     return copiedNodes[node.val]
+        
     def cloneGraph(self, node):
-        """
-        :type node: Node
-        :rtype: Node
-        """
+        oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+
+            newNode = Node(node.val)
+            oldToNew[node] = newNode
+
+            for neighbhor in node.neighbhors:
+                addNode = dfs(neighbhor)
+                newNode.neighbors.append(addNode)
+
+            return newNode
+        
+        return dfs(node) if node else None
+
+
