86ed4d96f9a7eff003e9c7c653a828b62878996f
Mohammed Abed <abedmohammed353@gmail.com>
2023-11-21 07:09:41 -0500

    Solved 43

--- 43MultiplyStrings.py
+++ 43MultiplyStrings.py
@@ -1,20 +1,29 @@
 class Solution(object):
     def multiply(self, num1, num2):
-        toAdd = []
+        if "0" in [num1, num2]:
+            return "0"
 
-        for i in range(len(num1) - 1, -1, -1):
-            carry = 0
-            section = "" + "0" * (len(num1) - i - 1)
-            for j in range(len(num2) - 1, -1, -1):
-                total = int(num1[i]) * int(num2[j]) + carry
-                section = str(total % 10) + section
-                carry = total // 10
-            if carry > 0:
-                section = str(carry) + section
-            toAdd.append(section)
+        answer = [0 for i in range(len(num1) + len(num2))]
 
-        return toAdd
+        for i2 in range(len(num2) - 1, -1, -1):
+            for i1 in range(len(num1) - 1, -1, -1):
+                res = int(num2[i2]) * int(num1[i1]) + answer[i1 + i2 + 1]
+                digit = res % 10
+                carry = res // 10
+
+                answer[i1 + i2 + 1] = digit
+                answer[i1 + i2] += carry
+
+        start = 0
+        for num in answer:
+            if num != 0:
+                break
+            start += 1
+
+        return "".join(str(num) for num in answer[start:])
 
 
 answer = Solution()
 print(answer.multiply(num1="123", num2="456"))
+print(answer.multiply(num1="341234", num2="23456"))
+print(answer.multiply(num1="0", num2="0"))
