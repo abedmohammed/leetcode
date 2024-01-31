78bdd054abc1c8370e943b2b24d925d713cec461
Mohammed Abed <abedmohammed353@gmail.com>
2024-01-31 06:42:40 -0500

    Leetcode daily

--- _repeats/739DailyTemperatures.py
+++ _repeats/739DailyTemperatures.py
@@ -1,6 +1,22 @@
 class Solution(object):
     def dailyTemperatures(self, temperatures):
+        stack = []
+        res = [0] * len(temperatures)
+
+        for i in range(len(temperatures) - 1, -1, -1):
+            curIndex = -1
+            while stack:
+                if stack[-1][0] > temperatures[i]:
+                    curIndex = stack[-1][1]
+                    break
+                else:
+                    stack.pop()
+
+            res[i] = curIndex - i if curIndex >= 0 else 0
+            stack.append((temperatures[i], i))
+
+        return res
 
 
 answer = Solution()
-print(answer.merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
+print(answer.dailyTemperatures(temperatures=[73, 74, 75, 71, 69, 72, 76, 73]))
