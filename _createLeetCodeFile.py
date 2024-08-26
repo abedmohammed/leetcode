1a03dcafd0638fc867e844f7c6d18fbb56d67e88
abedmohammed <abedmohammed353@gmail.com>
2024-08-26 06:41:41 -0400

    updating file locations

--- _createLeetCodeFile.py
+++ _createLeetCodeFile.py
@@ -1,23 +1,35 @@
 print("Question Name: ", end="")
 questionName = input()
 
-questionName = questionName.title().split()
-questionName[0] = questionName[0][:-1]
-questionName = "".join(questionName)
-questionName += ".py"
+# Process the question name for the file name
+fileNameParts = questionName.title().split()
+fileNameParts[0] = fileNameParts[0][:-1]
+fileName = "".join(fileNameParts)
+fileName += ".py"
 
-question = """class Solution(object):
-    def twoSum(self, nums, target):
-        seen = {}
+# Process the question name for the function name
+functionNameParts = questionName.lower().split()[
+    1:
+]  # Remove the number and make lowercase
+functionName = "".join(word.capitalize() for word in functionNameParts)
+functionName = (
+    functionName[0].lower() + functionName[1:]
+)  # Make the first character lowercase
+
+question = f"""class Solution(object):
+    def {functionName}(self, nums, target):
+        seen = {{}}
         for i in range(len(nums)):
             if(target - nums[i] in seen):
                 return [seen[target-nums[i]], i]
             seen[nums[i]] = i
 
 answer = Solution()
-print(answer.QUESTION(PARAMS))
+print(answer.{functionName}(PARAMS))
 """
 
-f = open(questionName, "w")
+f = open(fileName, "w")
 f.write(question)
 f.close()
+
+print(f"File '{fileName}' has been created with function name '{functionName}'.")
