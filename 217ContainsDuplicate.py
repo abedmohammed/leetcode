890a2702c1282ff86fb30dc76806e431f88c2f45
abedmohammed <abedmohammed353@gmail.com>
2024-12-20 06:41:02 -0500

    leetcode grind season 3

--- 217ContainsDuplicate.py
+++ 217ContainsDuplicate.py
@@ -1,10 +1,14 @@
 class Solution(object):
     def containsDuplicate(self, nums):
         seen = set()
+
         for num in nums:
             if num in seen:
                 return True
-            else:
-                seen.add(num)
+            seen.add(num)
 
         return False
+
+
+answer = Solution()
+print(answer.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
